4d91dea8b99b20121701d9cb6cab7450ba4a8ae1
moise <moise@192.168.1.6>
2025-04-24 21:59:02 -0600

    wacha lo que consegui

---
+++ src/semana/pkg1/progra/i/Semana1PrograI.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana.pkg1.progra.i;
+
+
+public class Semana1PrograI {
+    public static void main(String[] args) {
+        System.out.println("Progra 1 Semana 1 Bienvenido");
+        
+    }
+    
+}
